99482962ea53d390ebeb28a7605ad1636a3010e7
Shankar <91281182+shankar-4@users.noreply.github.com>
2022-03-27 10:04:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  msg = message.content
+#   msg = message.content
   if message.author == client.user:
     return
   if message.content.startswith('!hello'):
