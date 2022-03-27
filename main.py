c4cf31ce6976f9e1e69ae5277e9c4e80efcf67c9
Shankar <91281182+shankar-4@users.noreply.github.com>
2022-03-27 10:16:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ async def on_message(message):
 #   msg = message.content
   if message.author == client.user:
     return
+  
   if message.content.startswith('!hello'):
-    await message.channel.send('Hi,goood to see you')
+       await message.channel.send(f'Hello {message.author.name}!')
+    
   if message.content.startswith('!bye'):
-    await message.channel.send('Have a nice day')
+       await message.channel.send(f'Bye {message.author.name}! have a nice day')
+    
   if message.content.startswith('!agenda'):
     await message.channel.send('TTC navigate the culture of Open Source Collaboration. This club is being created by the students with interests to create greater opportunities for the students in their career further ahead')
+  
   if message.content.startswith('!goals'):
     await message.channel.send('''
 * Expose Members to Open - Source Community
@@ -37,6 +41,7 @@ async def on_message(message):
 * Improved coordination and focus of resources, and Better use of data.
 
 ''')
+    
   if  message.content.startswith('!current event'):
         await message.channel.send('''
   Current event going on is BOT-A-THON''')
@@ -47,12 +52,14 @@ async def on_message(message):
 * Interview Cake
 * Azure Developer
 * Codeflow - OpeForce 2022''')
+    
   if  message.content.startswith('!sig'):
         await message.channel.send('''
 SIG is a Special Intrest Group where students work on specific ways to contribute to the club .
 1.Intel SIG 
 2.Codestorming SIG
 3.Xploit SIG''')
+    
   if  message.content.startswith('!team'):
         await message.channel.send('''
 * Rishi R - Club Lead
@@ -64,6 +71,7 @@ SIG is a Special Intrest Group where students work on specific ways to contribut
 * Nishtha Jain - Co Pr Head 
 * Nihal Kadam - Bootcamp Coordinator
 * Aakash Rajaraman - Project Strategist''')
+    
   if  message.content.startswith('!help'):
         await message.channel.send(''' 
 * !team
@@ -76,6 +84,7 @@ SIG is a Special Intrest Group where students work on specific ways to contribut
 * !mentors
 * !hello
 * !bye''')
+    
   if  message.content.startswith('!social media'):
         await message.channel.send('''     
 Discord Server link: https://discord.gg/8aUecspTfC
@@ -93,6 +102,7 @@ Youtube - https://www.youtube.com/channel/UCoj5JIWPp2bPj99K2Yw293g
 Twitter - https://twitter.com/theturingclubJU
 
 Official website - https://theturingclub.in/ ''')
+    
   if  message.content.startswith('!mentors'):
         await message.channel.send('''    
 *  Dr. Chenraj Roychand 
@@ -101,8 +111,10 @@ Official website - https://theturingclub.in/ ''')
 *  Dr. V. Vivek 
 *  Prof. Roopashree S
 *  Jatinder Pal Singh Bagga ''')
+    
   if message.content.startswith('How are you'):
     await message.channel.send("I am a bot and i dont have feelings")
+    
   if message.content.startswith('Who are you'):
     await message.channel.send("I am TTC Help Bot and i will provide information regarding TTC club ")
 
